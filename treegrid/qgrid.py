0361a5eb49ef7c496b38c2e2fa39257858edab0f
quadrismegistus <ryan.heuser@gmail.com>
2015-05-29 11:35:33 -0700

    removing other unnecessary folders

--- treegrid/qgrid.py
+++
@@ -1,9 +0,0 @@
-import sys,pyparse
-
-try:
-	fn=sys.argv[1]
-except:
-	exit("[usage]: qgrid.py [xml_filename_of_stanford_corenlp_parser_output]")
-
-#ld=pyparse.parse2grid(fn,ldlim=10)
-pyparse.parse2tree(fn)

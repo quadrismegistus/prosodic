21fd1cbd0159e8f866587a9f21a4f799c5d4d70d
CubieDev <cubiegamedev@gmail.com>
2021-02-23 12:12:13 +0100

    Resolved issue with requirements not installing

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class PostInstallCommand(install):
 	"""Post-installation for installation mode."""
 	def run(self):
 		tools.configure_home_dir(force=True)
-		install.run(self)
+		install.do_egg_install(self)
 
 if __name__ == '__main__':
 	with open("README.md", "r", encoding="utf8") as fh:

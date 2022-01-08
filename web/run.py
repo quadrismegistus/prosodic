b0dd39f0ef36e874096fead61c0f8825a3402ed6
Ryan Heuser <ryan.heuser@gmail.com>
2022-01-07 21:02:03 -0500

    updates

--- web/run.py
+++ web/run.py
@@ -1,9 +1,9 @@
 #!bin/python
 
 import sys,os
-sys.path.insert(0,os.path.abspath('../..'))
-# import prosodic
-# print(prosodic.__file__)
+sys.path.insert(0,os.path.abspath('..'))
+import prosodic
+print(prosodic.__file__)
 
 from app import app,socketio
 socketio.run(app,host='0.0.0.0',port=5002,debug=True)

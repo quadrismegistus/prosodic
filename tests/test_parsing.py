f0dfa50cd0679cfef7b44a43432e6a63d70fb4ad
Ryan Heuser <ryan.heuser@gmail.com>
2023-10-01 20:05:59 -0400

    pruning

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,13 +1,18 @@
 import os,sys; sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from prosodic.imports import *
 
-def test_parsing():
+
+
+
+
+def test_feet():
     # iambic test
     tstr = 'embrace ' * 5
     l = Line(tstr)
     assert l.num_parses == 1
     assert l.best_parse.is_rising == True
     assert l.best_parse.nary_feet == 2
+    assert l.best_parse.foot_type == 'iambic'
 
     # trochaic test
     tstr = 'dungeon ' * 5
@@ -15,18 +20,20 @@ def test_parsing():
     assert l.num_parses == 1
     assert l.best_parse.is_rising == False
     assert l.best_parse.nary_feet == 2
+    assert l.best_parse.foot_type == 'trochaic'
 
     # anapestic
     tstr = 'disembark ' * 4
     l = Line(tstr)
     assert l.best_parse.is_rising == True
     assert l.best_parse.nary_feet == 3
+    assert l.best_parse.foot_type == 'anapestic'
 
     # dactylic test
     tstr = 'dangerous ' * 4
     l = Line(tstr)
     assert l.best_parse.is_rising == False
     assert l.best_parse.nary_feet == 3
+    assert l.best_parse.foot_type == 'dactylic'
     
 
-    

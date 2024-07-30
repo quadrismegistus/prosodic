c24716c402c9736f67c8b1f094d868ff2f3b1689
Ryan Heuser <ryan.heuser@gmail.com>
2024-07-30 18:39:32 +0100

    no quart needed?

--- prosodic/cli.py
+++ prosodic/cli.py
@@ -11,7 +11,7 @@ def cli():
 @click.option('--host', default='127.0.0.1', help='set host (127.0.0.1)')
 @click.option('--port', default=8181, help='set port (8181)')
 @click.option('--debug', is_flag=True, help='debug')
-def web(host='127.0.0.1', port=8181,debug=False):
+def web(host='127.0.0.1', port=8181, debug=False):
     from .web.app import main
     msg = f'Starting prosodic as a webserver at http://{host}:{port}...'
     click.echo(msg)
